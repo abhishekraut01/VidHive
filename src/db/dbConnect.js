7ddf7b525aa20045e48a55ceffe6be040cb37548
Abhishek <Abhishekraut8697@gmail.com>
2024-12-26 23:27:05 +0530

    app setup completed

--- src/db/dbConnect.js
+++ src/db/dbConnect.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
-export const connectDB = async ()=>{
-    try {
-        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI)
-        console.log("MongoDB connected successfully on "+connectionInstance.connection.host)
-    } catch (error) {
-        console.log("Something is wrong while connecting to database"+error)
-        process.exit(1)
-    }
-}
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URI, {
+    });
+    console.log(`MongoDB connected successfully on host: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to the database: ${error.message}`);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
 
-export default connectDB
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv'
-dotenv.config({
-    path :'./.env'
-})
-const app = express();
-import {connectDB} from './db/dbConnect.js'
+import dotenv from 'dotenv';
+import connectDB from './db/dbConnect.js';
+import app from './app.js';
+
+dotenv.config({ path: './.env' }); // Load environment variables
+
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(`Something went wrong while connecting to the database: ${err.message}`);
+    process.exit(1); // Exit process with failure code
+  });
