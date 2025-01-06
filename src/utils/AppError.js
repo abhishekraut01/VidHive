4fe2357a3d8ce911d0efce2750436efe84350a5e
Abhishek <Abhishekraut8697@gmail.com>
2025-01-06 21:57:06 +0530

    write custom apiResponse apperror and asynchandler

---
+++ src/utils/AppError.js
@@ -0,0 +1,23 @@
+class AppError extends Error {
+    constructor(
+        message = "Internal server error", // Default message
+        statusCode = 500, // Default status code
+        errors = [], // Optional array for additional error details
+        stack = '' // Optional custom stack trace
+    ) {
+        super(message); // Call the parent Error constructor
+        this.statusCode = statusCode; // HTTP status code
+        this.success = false; // For API response structure
+        this.errors = errors; // Additional error details
+        this.isOperational = true; // Distinguish operational vs programming errors
+
+        // Handle the stack trace
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor); // Auto-capture stack trace
+        }
+    }
+}
+
+module.exports = AppError;
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,17 @@
+
+const asyncHandler =(fn)=>{
+    return async (req,res,next)=>{
+        try {
+            await fn(req,res,next)
+        } catch (error) {
+            console.log(error)
+            const statusCode = error.statusCode || 500
+            const message = error.message
+
+            return res.status(statusCode).json({
+                success : false,
+                message: message,
+            })
+        }
+    }
+}
